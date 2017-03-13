451e5cd0296a20592d59d42ff290f6b17c0134f6
Carlos Silva <charlesipn@gmail.com>
2017-03-13 02:02:32 -0600

    basic webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,34 @@
-const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+var webpack = require('webpack');
+var path = require('path');
 
-const config = {
-  entry: './index.js',
+var BUILD_DIR = path.resolve(__dirname, 'built');
+var APP_DIR = path.resolve(__dirname, 'src');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: BUILD_DIR,
     filename: 'bundle.js'
-  }
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.(js|jsx)$/,
+        include : APP_DIR,
+        loader : 'babel-loader'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "built"),
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+    new HtmlWebpackPlugin({template: './src/index.html'})
+  ]
 };
+
 module.exports = config;
