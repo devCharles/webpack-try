587e5e19edd4b5028dec9155be1fb0ad8eeca69c
Carlos Silva <charlesipn@gmail.com>
2017-03-13 00:01:22 -0600

    initialized

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+const config = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  }
+};
+module.exports = config;
